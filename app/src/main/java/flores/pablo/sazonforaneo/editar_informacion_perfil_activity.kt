09bf6cbf0b5ae534f169297a0d9a6576ae0d751c
AMOZINGAS <amosheli2004@gmail.com>
2025-06-24 15:42:20 -0600

    Se agrego la actividad de informacion personal

---
+++ app/src/main/java/flores/pablo/sazonforaneo/editar_informacion_perfil_activity.kt
@@ -0,0 +1,16 @@
+package flores.pablo.sazonforaneo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class editar_informacion_perfil_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_editar_informacion_perfil)
+
+    }
+}
